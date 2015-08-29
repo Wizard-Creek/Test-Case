5e365277892828c3e45672bde4d1a871aa7b842e
Les Bain <les.bain@gmail.com>
2015-08-29 10:40:46 -0700

    Getting ready to merge or pull down

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+session_start();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
+<!-- #BeginTemplate "dwt/admin-system.dwt" -->
 <head>
+<?php
+ini_set("include_path", ".:../:./includes:../includes:./content:../content:../../content:../../../content");
+include "html-header.php";
+?>
+<!-- #BeginEditable "scripts" -->
+<style type="text/css">
+</style>
+<!-- #EndEditable -->
+<?php
+include "google-analytics.php";
+?>
 </head>
-
 <body>
+<?php
+include "page-header.php";
+?>
+<div class="bodyContent">
+<!-- #BeginEditable "body" --> 
+&nbsp;
+<!-- #EndEditable -->
+</div>
+
+<?php
+include "page-footer.php";
+?>
 
 </body>
-
+<!-- #EndTemplate -->
 </html>
